5aa11199cc5a897d01fff44cd93d8b5937510eec
zanerebouded@gmail.com <TheUnquiet>
2025-04-26 21:49:21 +0200

    Comment voor vraag 3

--- main.py
+++ main.py
@@ -6,36 +6,41 @@ from pathlib import Path
 from Models.Molecule import Molecule
 
 # PART 3 START
-TDDFTTDA_list = []
-TDDFT_list = []
+TDDFTTDA_list = [] # Alle TDDFT/TDA" waarden
+TDDFT_list = [] # Alle TDDFT waarden
 
 files_dir = "files"
 
-dat_files = list(Path(files_dir).rglob("*.dat"))
-out_files = list(Path(files_dir).rglob("*.out"))
-
+dat_files = list(Path(files_dir).rglob("*.dat")) # Alle .dat bestanden
+out_files = list(Path(files_dir).rglob("*.out")) # Alle .out bestanden
+# We gaan door 1 van de lijsten 
+# We hebben enkel de idx van de loop nodig, de lijsten zijn even groot door welke lijst je loopt maakt niet uit
 for idx, file in enumerate(dat_files):
+    # We maken een molucule object voor elke molecule
+    # We geven de .out- en .dat bestand mee
     molucule = Molecule(str(out_files[idx]), str(dat_files[idx]))
 
-    TDDFTTDA_list.extend(molucule.returnPossibleExcitedState("TDDFT/TDA"))
-    TDDFT_list.extend(molucule.returnPossibleExcitedState("TDDFT"))
-
-TDDFTTDA_array = np.array(TDDFTTDA_list).reshape(-1, 1)
+    # We gaan hier de TDDFT/TDA waarden toevoegen aan onze grote lijst ( zie lijn 9 )
+    TDDFTTDA_list.extend(molucule.return_possible_excited_states("TDDFT/TDA"))
+    # We gaan hier de TDDFT waarden toevoegen aan onze grote lijst ( zie lijn 10 )
+    TDDFT_list.extend(molucule.return_possible_excited_states("TDDFT"))
+# We maken van de grote lijsten Numpy Arrays deze zijn sneller + scikit-learn accepteert alleen Numpy arrays
+TDDFTTDA_array = np.array(TDDFTTDA_list).reshape(-1, 1) # We zetten de lijst om naar een 2D array ( LinearRegression().fit(X, y) accepteert enkel 2D arrays voor X)
 TDDFT_array = np.array(TDDFT_list)
 
-model = LinearRegression()
-model.fit(TDDFTTDA_array, TDDFT_array)
+model = LinearRegression() # We maken onze regressie model
+model.fit(TDDFTTDA_array, TDDFT_array) # Trein het model ( Hij gaat gwn die waarden doornemen en dan kan hij later een voorspelling maken "dalen of stijgen ze?" )
 
-y_pred = model.predict(TDDFTTDA_array)
+y_pred = model.predict(TDDFTTDA_array) # Hier maakt hij dan de voorspelling
 
-diff = model.score(TDDFTTDA_array, TDDFT_array)  
-
-plt.scatter(TDDFTTDA_array, TDDFT_array, label="Molecullen")
-plt.plot(TDDFTTDA_array, y_pred, color='red', label=f'R^2 = {diff:.3f}')
-plt.xlabel('TDDFT/TDA excitation energies (eV)')
-plt.ylabel('TDDFT excitation energies (eV)')
-plt.legend()
-plt.grid(True)
-plt.show()
+diff = model.score(TDDFTTDA_array, TDDFT_array) # Dit is onze R² (determinatiecoëfficiënt)
+# Hier gaan we het plot tekenen
+plt.scatter(TDDFTTDA_array, TDDFT_array, label="Molecullen") # Ik toon de molecullen ( ik ben niet zeker of het moet )
+plt.plot(TDDFTTDA_array, y_pred, color='red', label=f'R^2 = {diff:.3f}') # Ik toon de waarde van R²
+plt.xlabel('TDDFT/TDA excitation energies (eV)') # Wat betekenen de waarden van de X as?
+plt.ylabel('TDDFT excitation energies (eV)') # Wat betekenen de waarden van de Y as?
+plt.legend() # Toon zo een box met de betekenis van de kleurtjes 😎
+plt.grid(True) # Een grid, de vakjes
+plt.show() # Toon de plot 😎
 
 # PART 3 FINISH
