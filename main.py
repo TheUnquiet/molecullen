aa81d4a77c85a0752c9a0c8381167329a1dc0529
zanerebouded@gmail.com <TheUnquiet>
2025-04-26 20:50:58 +0200

    Vraag 3

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from pathlib import Path
 from Models.Molecule import Molecule
-# PART 1 START
-m = Molecule("files/Cyanidin_H_tddft_short.out", "files/Cyanidin_H_tddft_full_simspec.dat");
 
-# PART 1 FINSIH
+# PART 3 START
+TDDFTTDA_list = []
+TDDFT_list = []
 
-# PART 2 START
-print("TDDFT Energies eV")
+files_dir = "files"
 
-for i in m.returnPossibleExcitedState("TDDFT"):
-    print(i)
+dat_files = list(Path(files_dir).rglob("*.dat"))
+out_files = list(Path(files_dir).rglob("*.out"))
 
-print("TDDFT/TDA Energies eV")
+for idx, file in enumerate(dat_files):
+    molucule = Molecule(str(out_files[idx]), str(dat_files[idx]))
 
-for i in m.returnPossibleExcitedState("TDDFT/TDA"):
-    print(i)
+    TDDFTTDA_list.extend(molucule.returnPossibleExcitedState("TDDFT/TDA"))
+    TDDFT_list.extend(molucule.returnPossibleExcitedState("TDDFT"))
 
-# PART 2 FINISH
+TDDFTTDA_array = np.array(TDDFTTDA_list).reshape(-1, 1)
+TDDFT_array = np.array(TDDFT_list)
+
+model = LinearRegression()
+model.fit(TDDFTTDA_array, TDDFT_array)
+
+y_pred = model.predict(TDDFTTDA_array)
+
+diff = model.score(TDDFTTDA_array, TDDFT_array)  
+
+plt.scatter(TDDFTTDA_array, TDDFT_array, label="Molecullen")
+plt.plot(TDDFTTDA_array, y_pred, color='red', label=f'R^2 = {diff:.3f}')
+plt.xlabel('TDDFT/TDA excitation energies (eV)')
+plt.ylabel('TDDFT excitation energies (eV)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# PART 3 FINISH
